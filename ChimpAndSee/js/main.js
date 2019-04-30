d8ea3775398d489be589c205054fddf4aebb0da6
Will Granger <twgranger@gmail.com>
2019-04-30 15:06:30 -0500

    Declare checkPath as function

--- ChimpAndSee/js/main.js
+++ ChimpAndSee/js/main.js
@@ -1,4 +1,4 @@
-const checkPath = function() {
+function checkPath() {
   const path = "/#/about/authors";
   const location = window.location.href;
   const redirectUrl = "https://www.zooniverse.org/projects/sassydumbledore/chimp-and-see/about/team";
