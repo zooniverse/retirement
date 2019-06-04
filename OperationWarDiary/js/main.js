906d35f1f82b1abd9c51d8a8bd4a720057cb68fd
Will Granger <twgranger@gmail.com>
2019-06-04 09:53:44 -0500

    Remove Chimp and See References

--- OperationWarDiary/js/main.js
+++
@@ -1,9 +0,0 @@
-function checkPath() {
-  const path = "/#/about/authors";
-  const location = window.location.href;
-  const redirectUrl = "https://www.zooniverse.org/projects/sassydumbledore/chimp-and-see/about/team";
-
-  if (location.indexOf(path) >= 0) {
-    window.location.replace(redirectUrl);
-  }
-}
